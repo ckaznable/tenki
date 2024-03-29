5aadd3ebf78368d9d451ad38ea3057d805f52a29
ckaznable <ckaznable@gmail.com>
2024-03-29 15:24:35 +0800

    refactor: extract buf to new module impl

---
+++ src/state/buffer.rs
@@ -0,0 +1,33 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
+use ratatui::layout::Rect;
+use tinyvec::ArrayVec;
+
+use super::DropColumn;
+use super::DropSpeed;
+
+pub struct RenderBuffer {
+    pub buf: Vec<DropColumn>,
+    pub line: Vec<DropSpeed>,
+}
+
+impl RenderBuffer {
+    pub fn new(size: Rect) -> Self {
+        let mut buf = Vec::with_capacity(size.width as usize);
+        for _ in 0..size.width {
+            let mut column = Vec::with_capacity(size.height as usize);
+            for _ in 0..size.height {
+                column.push(ArrayVec::<[DropSpeed; 3]>::default());
+            }
+
+            buf.push(Rc::new(RefCell::new(column)));
+        }
+
+        Self {
+            line: Vec::with_capacity(buf.len()),
+            buf,
+        }
+    }
+}
+
